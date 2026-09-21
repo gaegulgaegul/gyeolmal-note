7f0aec4235fc2f78ed6ab98e3def0d75e3f97745
gaegulgaegul <dla9721@gmail.com>
2026-09-21 23:31:33 +0900

    Initial commit: 결말노트 - AstroPaper 기반 OTT 결말 해석 블로그

---
+++ astro-paper.config.ts
@@ -0,0 +1,42 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://gyeolmalnote.com/",
+    title: "결말노트",
+    description: "넷플릭스, 티빙 등 OTT 오리지널 시리즈의 결말을 정리하고 해석하는 블로그입니다.",
+    author: "결말노트",
+    profile: "https://gyeolmalnote.com/about",
+    ogImage: "default-og.jpg",
+    lang: "ko",
+    timezone: "Asia/Seoul",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: true,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: false,
+    },
+    search: "pagefind",
+  },
+  socials: [
+    { name: "github", url: "https://github.com/gaegulgaegul/gyeolmal-note" },
+    { name: "mail",   url: "mailto:contact@gyeolmalnote.com" },
+  ],
+  shareLinks: [
+    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "telegram", url: "https://t.me/share/url?url=" },
+    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
